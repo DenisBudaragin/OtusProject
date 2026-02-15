67fb0b6e02b61604c5593ac2d4c105f2e99c08f5
Denis <budaragindenis@mail.ru>
2026-02-15 15:03:11 +0300

    logic refactoring

--- src/main/java/configa/Config.java
+++ src/main/java/configa/Config.java
@@ -4,14 +4,11 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class Config {
-//    public static final String CATALOG_URL = "https://otus.ru/catalog/courses";
-
     public static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern("d MMMM, yyyy", new Locale("ru"));
 
-    public static final String COURSE_NAME = "Solidity Developer"; // Замените на имя курса для поиска
-    public static final String COURSE_XPATH = "//h6/div[contains(text(), 'Solidity Developer')]"; // Замените на имя курса для поиска
+    public static final String COURSE_NAME = "Reinforcement Learning";
+    public static final String COURSE_XPATH = "//h6/div[contains(text(), 'Reinforcement Learning')]";
     public static final String OTUS_MAIN_PAGE = "https://otus.ru";
     public static final String OTUS_COURSES_PAGE = "https://otus.ru/catalog/courses";
-    public static final String CATEGORY_NAME = "Обучение";
 }
