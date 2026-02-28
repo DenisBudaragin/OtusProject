6f41ce631f4a03953eca15ccebb98c9ecbb6139d
Denis <budaragindenis@mail.ru>
2026-02-28 14:43:48 +0300

    logic refactoring

--- src/main/java/configa/Config.java
+++ src/main/java/configa/Config.java
@@ -7,8 +7,8 @@ public class Config {
     public static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern("d MMMM, yyyy", new Locale("ru"));
 
-    public static final String COURSE_NAME = "Reinforcement Learning";
-    public static final String COURSE_XPATH = "//h6/div[contains(text(), 'Reinforcement Learning')]";
+    public static final String COURSE_NAME = "Промпт-инжиниринг: внедрение ИИ в бизнес-процессы";
+    public static final String COURSE_XPATH = "//h6/div[contains(text(), 'Промпт-инжиниринг: внедрение ИИ в бизнес-процессы')]";
     public static final String OTUS_MAIN_PAGE = "https://otus.ru";
     public static final String OTUS_COURSES_PAGE = "https://otus.ru/catalog/courses";
 }
